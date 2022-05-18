0ae1970a94c68e976f4e453ce7ac7a8259ea0301
Grant Belcher <gtbelch@gmail.com>
2022-05-18 10:50:11 -0700

    moves post to seperate container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,54 +8,6 @@ import {
   DotsThree,
 } from "phosphor-react";
 
-const App = () => (
-  <>
-    <div className="post-list">
-      <Post>
-        <Post.Banner>
-          <Post.Header>
-            <Post.ProfilePic></Post.ProfilePic>
-            <Post.Info>
-              <Post.Author>destinationhiking</Post.Author>
-              <Post.Location>
-                Havasu Falls, Supai, AZ - Grand Canyon
-              </Post.Location>
-            </Post.Info>
-          </Post.Header>
-          <Post.Options>
-            <Post.OptionsBtn>
-              <DotsThree size={32} />
-            </Post.OptionsBtn>
-          </Post.Options>
-        </Post.Banner>
-        <Post.Image src="/images/hood.jpg" alt="falls" />
-        <Post.Body>
-          <Post.Icons>
-            <Post.IconGroup>
-              <Heart size={30} />
-              <ChatCircle size={30} />
-              <PaperPlaneTilt size={30} />
-            </Post.IconGroup>
-            <Post.IconGroup>
-              <BookmarkSimple size={30} />
-            </Post.IconGroup>
-          </Post.Icons>
-          <Post.Likes>37955 likes</Post.Likes>
-          <Post.Caption>
-            <a className="handle">grantb69</a> Round 4 of the Sydney Surf Pro
-            Longboard is on and @harrisonsan has hit the water....{" "}
-            <a className="expand">more</a>
-          </Post.Caption>
-          <Post.Comments>View all 21 comments</Post.Comments>
-          <Post.PostedAt>4 hours ago</Post.PostedAt>
-          <Post.NewComment>
-            <Post.Input />
-            <Post.Submit>Post</Post.Submit>
-          </Post.NewComment>
-        </Post.Body>
-      </Post>
-    </div>
-  </>
-);
+const App = () => <>App</>;
 
 export default App;
